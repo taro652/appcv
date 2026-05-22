8ac55be83cdca4d97006b08d76909f69ac161f02
taro652 <lautaro360zerda@gmail.com>
2026-05-21 23:01:37 -0300

    Carga y parseo de pdf, botones menos redondos

--- frontend/scripts/scrips.js
+++ frontend/scripts/scrips.js
@@ -1,55 +1,100 @@
-/*import * as pdfjsLib from 'pdfjs-dist';
+import * as pdfjsLib from 'pdfjs-dist';
+import pdfWorker from 'pdfjs-dist/build/pdf.worker.mjs?url';
 
-const input = document.getElementById('file-input');
-const sendBtn = document.getElementById('send-btn');
-const clearBtn = document.getElementById('clear-btn');
-const preview = document.querySelector('.preview');
-
-let selectedFile = null; //variable para manipular el archivo en memoria
-
-// 1. Capturar y manipular el archivo al seeleccionarlo
-input.addEventListener('change', (e) => {
-    const file = e.target.files[0];
-
-    if(!file) return;
-
-    selectedFile = file; //guarda la referencia  en memoria
-    sendBtn.disabled = false; //habilita los botones
-    clearBtn.disabled = false;
-
-    //Genear preview según el tipo
-    if(file.type === file.name.endsWith('.pdf')) {
-        const fileURL = URL.createObjectURL(file);
-        preview.innerHTML = '<iframe src="${fileURL}"></iframe>';
-        console.log("wsdgfdshg");
-    } else if (file.type === file.name.endsWith('.docx')) {
-        preview.innerHTML = '<div class="doc-preview"> <strong>Archivo Word detectado:</strong><br> ${file.name}<br> <small>(${ (file.size / 1024).toFixed(1) } KB)</small> </div>';
-    } else {
-        preview.innerHTML = '<span style="color: red;">Formato no soportado</span>';
-    }
-    })
-});
-
-*/
+pdfjsLib.GlobalWorkerOptions.workerSrc = pdfWorker;
 
 const input = document.getElementById("file-input");
 const preview = document.querySelector(".preview");
 const clearBtn = document.getElementById('clear-btn');
+const sendBtn = document.getElementById('send-btn');
+const info = document.querySelector('.info');
 
+let selectedFile = null; //variable para manipular el archivo en memoria
 
-input.addEventListener("change", () => {
+// 1. Capturar y manipular el archivo al seeleccionarlo
+input.addEventListener('change', () => {
     const file = input.files[0];
 
-        if (!file) return;
+    if(!file) return;
 
+    // selectedFile = file; //guarda la referencia en memoria
+    sendBtn.disabled = false; //habilita los botones
+    clearBtn.disabled = false;
+    console.log(file.type);
+    
+    if(file.type === "application/pdf") {
         const url = URL.createObjectURL(file);
-
-            preview.innerHTML = `<embed src="${url}"  type="application/pdf" width="100%" height="100%"/>`;
-            console.log(file);
-            console.log(file.type);
+        preview.innerHTML = `<embed src="${url}"  type="application/pdf" width="100%" height="100%"/>`;
         
-        clearBtn.addEventListener('click',() => {
+        parsePDF(file).then(data => {
+            info.innerHTML = "Esto es lo que analizará la IA:</br></br></br>" + data;
+        });
+
+        sendBtn.style.opacity = 100;
+    } else {
+        preview.innerHTML = '<span style="color: red;">Formato no soportado</span>';
+    }
+
+    clearBtn.addEventListener('click',() => {
         input.value = "";
         preview.innerHTML = ''
-    })
+        info.innerHTML = '';
+        sendBtn.style.opacity = 0;
+        clearBtn.disabled = true;
+        sendBtn.disabled = true;
+    });
+
+    sendBtn.addEventListener("click", () => {
+        sendBtn();
+    });
 });
+
+function send() {
+    fetch("/api/review", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            cvText: info.innerHTML
+        })
+    })
+}
+
+async function parsePDF(file) {
+    const arrayBuffer = await file.arrayBuffer()
+
+    const pdf = await pdfjsLib.getDocument({
+        data: arrayBuffer
+    }).promise
+
+    let text = ""
+
+    for (let i = 1; i <= pdf.numPages; i++) {
+        const page = await pdf.getPage(i)
+
+        const content = await page.getTextContent()
+
+        const strings = content.items.map(item => item.str)
+
+        text += strings.join(" ")
+    }
+
+
+    text = text.replace(
+        /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/gi,
+        "[EMAIL]"
+    )
+
+    text = text.replace(
+        /\+?\d[\d\s\-()]{7,}/g,
+        "[TELEFONO]"
+    )
+
+    text = text.replace(
+        /https?:\/\/(www\.)?linkedin\.com\/\S+/gi,
+        "[LINKEDIN]"
+    )
+
+    return text
+}
